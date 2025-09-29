f536357ca47fd0400fd5280195ce2440a981e834
Clemens Hahn <clemens@couchbits.com>
2025-09-29 11:44:43 +0200

    Refactor: Replace standalone functions with `fs` package equivalents for improved consistency and readability.

--- R/shiny_bookmark_handler.R
+++ R/shiny_bookmark_handler.R
@@ -1,30 +1,27 @@
-library("fs")
-library("jsonlite")
-
 bookmarkRootDir = "shiny_bookmarks" # it seems impossible to change the root-dir :/
 bookmarkDir <- paste0(bookmarkRootDir, "/latest")
 # `input.rds` is the expected file name by shiny!
 bookmarkFileName <- "input.rds"
-bookmarkRdsTargetPath <- path(bookmarkDir, bookmarkFileName)
+bookmarkRdsTargetPath <- fs::path(bookmarkDir, bookmarkFileName)
 # `input.json` is a custom file-name and a custom file-content to access the shiny values in plain text
 bookmarkJsonName <- "input.json"
-bookmarkJsonTargetPath <- path(bookmarkDir, bookmarkJsonName)
+bookmarkJsonTargetPath <- fs::path(bookmarkDir, bookmarkJsonName)
 
 saveBookmarkAsLatest <- function(url) {
   stateId <- parseQueryString(sub("^.*\\?", "", url))$`_state_id_`
-  if(!dir_exists(bookmarkDir)){
-    dir_create(bookmarkDir)
+  if(!fs::dir_exists(bookmarkDir)){
+    fs::dir_create(bookmarkDir)
   }
-  file_move(
-    path = path("shiny_bookmarks", stateId, bookmarkFileName),
+  fs::file_move(
+    path = fs::path("shiny_bookmarks", stateId, bookmarkFileName),
     new_path = bookmarkRdsTargetPath
   )
-  dir_delete(path("shiny_bookmarks", stateId))
+  fs::dir_delete(fs::path("shiny_bookmarks", stateId))
   logger.debug(paste("[bookmark] Moved shiny bookmark", stateId, "to", bookmarkDir))
 }
 
 restoreShinyBookmark <- function(session) {
-  if(file_exists(bookmarkRdsTargetPath) && is.null(parseQueryString(session$clientData$url_search)$`_state_id_`)) {
+  if(fs::file_exists(bookmarkRdsTargetPath) && is.null(parseQueryString(session$clientData$url_search)$`_state_id_`)) {
     updateQueryString(queryString = "?_state_id_=latest")
     logger.debug("[bookmark] Reloading session b/c of detected (not yet loaded) shiny bookmark")
     session$reload()
